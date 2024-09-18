73c9911d150661db2dedf40d934252fa2b56dc63
Yudhajit Chowdhury <yudhajitchowdhury17@gmail.com>
2024-09-19 02:16:06 +0530

    Introduced Navigation for SPA

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useState } from "react";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
@@ -9,6 +9,8 @@ import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import { useEffect } from "react";
 import CountryList from "./components/CountryList";
+import City from "./components/City";
+import Form from "./components/Form";
 
 const URL = "http://localhost:8000";
 
@@ -42,14 +44,15 @@ function App() {
           <Route path="app" element={<AppLayout />}>
             <Route
               index
-              element={<CityList cities={cities} isLoading={isLoading} />}
+              element={<Navigate replace to='cities'/>}
             />
             <Route
               path="cities"
               element={<CityList cities={cities} isLoading={isLoading} />}
             />
+            <Route path="cities/:id" element={<City/>}/>
             <Route path="countries" element={<p><CountryList cities = {cities} isLoading={isLoading}/></p>} />
-            <Route path="form" element={<p>Form</p>} />
+            <Route path="form" element={<Form/>} />
           </Route>
           <Route path="login" element={<Login />} />
           <Route path="*" element={<PageNotFound />} />
